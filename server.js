ad38097419c0ffba0a46ebd04edec47263996fa0
Jei Pantoja <jmypaca@gmail.com>
2022-05-25 23:42:00 -0500

    footer

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const app = express()
 // Le decimos a ese App que sirva todos los archivos en la carpeta /public
 app.use(express.static('src'));
 // Indicamos al server que debe escuchar en el puerto 3000 🚀
-app.listen(3000 , () => { console.log( "app running on port 3000 🚀" )})
+app.listen(3000 , () => {
+    console.log( "app running on port 3000 🚀" )
+    console.log( "ve al navegador y coloca localhost:3000" )
+    console.log( "ahora configura scss, abre otra rerminal y" )
+    console.log( "rpn run sass" )
+    })
